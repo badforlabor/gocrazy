7a78a81e2c56d6a90d6120563c220c8d2ce79420
liubo04 <liubo04@work.com>
2019-12-25 17:44:56 +0800

    唯一id

---
+++ alg/id.go
@@ -0,0 +1,27 @@
+/**
+ * Auth :   liubo
+ * Date :   2019/12/25 17:42
+ * Comment: 唯一ID
+ */
+
+package alg
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/binary"
+	"fmt"
+	"time"
+)
+
+// 根据当前时间进行sha1计算，取前7位。（仿照git）
+func getShortId() string {
+
+	hash := sha1.New()
+	buff := new(bytes.Buffer)
+	binary.Write(buff, binary.LittleEndian, time.Now().UnixNano())
+	hash.Write(buff.Bytes())
+	s := fmt.Sprintf("%016x", hash.Sum(nil))
+
+	return s
+}
