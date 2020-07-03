dfe2b647b0cf4a63ec1aba3149812a9dd432bfa4
liubo04 <liubo04@work.com>
2020-07-03 16:29:54 +0800

    拷贝文件

--- crazyio/path.go
+++ crazyio/path.go
@@ -7,8 +7,10 @@
 package crazyio
 
 import (
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // d:/folder1/ -> folder1
@@ -57,4 +59,79 @@ func RemoveAll(wildcardPath string)  {
 			}
 		}
 	}
+}
+
+// 拷贝文件，或者文件夹
+func CopyPath(srcFullpath, dstFullpath string) error {
+	var f, e = os.Stat(srcFullpath)
+	if e != nil {
+		return e
+	}
+	if f.IsDir() {
+		e = copyDir(srcFullpath, dstFullpath)
+	} else {
+		e = copyFile(srcFullpath, dstFullpath)
+	}
+	return
+}
+
+func pathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+func copyFile(src, dst string) (w int64, err error) {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer srcFile.Close()
+	dstSlices := strings.Split(dst, string(os.PathSeparator))
+	dstSlicesLen := len(dstSlices)
+	destDir := ""
+	for i := 0; i < dstSlicesLen-1; i++ {
+		destDir = destDir + dstSlices[i] + string(os.PathSeparator)
+	}
+	b, err := pathExists(destDir)
+	if b == false {
+		err := os.Mkdir(destDir, os.ModePerm) //在当前目录下生成md目录
+		if err != nil {
+			return
+		}
+	}
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+
+	defer dstFile.Close()
+	return io.Copy(dstFile, srcFile)
+}
+
+func copyDir(src string, dest string) error {
+	srcOriginal := src
+	// 否则使用'ioutil.ReadDir'来自行逐层遍历!
+	err := filepath.Walk(src, func(src string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+		filePart, _ := filepath.Rel(srcOriginal, src)
+		destNew := filepath.Join(dest, filePart)
+
+		// 注意:这里会walk所有得文件!(而不仅仅是当前层级得)
+		if !f.IsDir() {
+			_, err := copyFile(src, destNew)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	return err
 }
