6568972fae9e3400da1d688a4b1aeed3f722d032
liubo04 <liubo04@work.com>
2021-04-14 14:49:25 +0800

    日志

--- crazy3rd/glog/glogCheater.go
+++ crazy3rd/glog/glogCheater.go
@@ -4,26 +4,39 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"strings"
 )
 
 var dir string = ""
 
+var myLogger *Logger
+
+var LogFileName = "example.log"
+
 func BaseInit() {
 	dir := getCurPath()
-	dir = strings.Join([]string{dir, "logs"}, string(os.PathSeparator))
+	dir = path.Join(dir, "logs")
 	os.Mkdir(dir, os.ModeDir)
+
+	var cfg = Config{ConsoleLoggingEnabled:true, EncodeLogsAsJson:false,
+		FileLoggingEnabled:true, Directory:dir, Filename:LogFileName,
+		MaxBackups:100,MaxAge:7, MaxSize:32}
+	myLogger = Configure(cfg)
 
 	Cheat(dir)
 
-	MaxSize = 1024 * 1024 * 16
+	//MaxSize = 1024 * 1024 * 16
+}
+func Close() {
+
 }
 
 func Cheat(tmpdir string) {
 	dir = tmpdir
 	// 设置输出路径
-	logDir = &dir
+	//logDir = &dir
 }
 
 func getCurPath() string {
@@ -45,4 +58,108 @@ func getCurPath() string {
 	var rst = strings.Join(splitstring, string(os.PathSeparator))
 
 	return rst
+}
+
+// Flush flushes all pending log I/O.
+func Flush() {
+
+}
+
+// Info logs to the INFO log.
+// Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
+func Info(args ...interface{}) {
+	myLogger.Info().Msg(fmt.Sprint(args...))
+}
+
+// InfoDepth acts as Info but uses depth to determine which call frame to log.
+// InfoDepth(0, "msg") is the same as Info("msg").
+func InfoDepth(depth int, args ...interface{}) {
+	myLogger.Info().Msg(fmt.Sprint(args...))
+}
+
+// Infoln logs to the INFO log.
+// Arguments are handled in the manner of fmt.Println; a newline is appended if missing.
+func Infoln(args ...interface{}) {
+	myLogger.Info().Msg(fmt.Sprintln(args...))
+}
+
+// Infof logs to the INFO log.
+// Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
+func Infof(format string, args ...interface{}) {
+	myLogger.Info().Msg(fmt.Sprintf(format, args...))
+}
+
+// Warning logs to the WARNING and INFO logs.
+// Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
+func Warning(args ...interface{}) {
+	myLogger.Warn().Msg(fmt.Sprint(args...))
+}
+
+// WarningDepth acts as Warning but uses depth to determine which call frame to log.
+// WarningDepth(0, "msg") is the same as Warning("msg").
+func WarningDepth(depth int, args ...interface{}) {
+	myLogger.Warn().Msg(fmt.Sprint(args...))
+}
+
+// Warningln logs to the WARNING and INFO logs.
+// Arguments are handled in the manner of fmt.Println; a newline is appended if missing.
+func Warningln(args ...interface{}) {
+	myLogger.Warn().Msg(fmt.Sprintln(args...))
+}
+
+// Warningf logs to the WARNING and INFO logs.
+// Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
+func Warningf(format string, args ...interface{}) {
+	myLogger.Warn().Msg(fmt.Sprintf(format, args...))
+}
+
+// Error logs to the ERROR, WARNING, and INFO logs.
+// Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
+func Error(args ...interface{}) {
+	myLogger.Error().Msg(fmt.Sprint(args...))
+}
+
+// ErrorDepth acts as Error but uses depth to determine which call frame to log.
+// ErrorDepth(0, "msg") is the same as Error("msg").
+func ErrorDepth(depth int, args ...interface{}) {
+	myLogger.Error().Msg(fmt.Sprint(args...))
+}
+
+// Errorln logs to the ERROR, WARNING, and INFO logs.
+// Arguments are handled in the manner of fmt.Println; a newline is appended if missing.
+func Errorln(args ...interface{}) {
+	myLogger.Error().Msg(fmt.Sprintln(args...))
+}
+
+// Errorf logs to the ERROR, WARNING, and INFO logs.
+// Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
+func Errorf(format string, args ...interface{}) {
+	myLogger.Error().Msg(fmt.Sprintf(format, args...))
+}
+
+// Fatal logs to the FATAL, ERROR, WARNING, and INFO logs,
+// including a stack trace of all running goroutines, then calls os.Exit(255).
+// Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
+func Fatal(args ...interface{}) {
+	myLogger.Fatal().Msg(fmt.Sprint(args...))
+}
+
+// FatalDepth acts as Fatal but uses depth to determine which call frame to log.
+// FatalDepth(0, "msg") is the same as Fatal("msg").
+func FatalDepth(depth int, args ...interface{}) {
+	myLogger.Fatal().Msg(fmt.Sprint(args...))
+}
+
+// Fatalln logs to the FATAL, ERROR, WARNING, and INFO logs,
+// including a stack trace of all running goroutines, then calls os.Exit(255).
+// Arguments are handled in the manner of fmt.Println; a newline is appended if missing.
+func Fatalln(args ...interface{}) {
+	myLogger.Fatal().Msg(fmt.Sprintln(args...))
+}
+
+// Fatalf logs to the FATAL, ERROR, WARNING, and INFO logs,
+// including a stack trace of all running goroutines, then calls os.Exit(255).
+// Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
+func Fatalf(format string, args ...interface{}) {
+	myLogger.Fatal().Msg(fmt.Sprintf(format, args...))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/badforlabor/gocrazy
 
 go 1.13
 
-require github.com/stretchr/testify v1.4.0
+require (
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/lestrrat-go/strftime v1.0.4 // indirect
+	github.com/rs/zerolog v1.21.0
+	github.com/stretchr/testify v1.4.0
+)
